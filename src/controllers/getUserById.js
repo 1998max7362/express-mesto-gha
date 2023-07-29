9b97336cc091cfa8175d74908c7a1f7241dca1f5
Max <1998max7362@mail.ru>
2023-07-30 02:47:00 +0300

    getUserById

--- src/controllers/getUserById.js
+++ src/controllers/getUserById.js
@@ -1,5 +1,12 @@
+import user from "../models/user.js";
 
-
-export const getUserById = (req,res)=>{
-
-}
+export const getUserById = async (req, res) => {
+  const userId = req.params.userId
+  console.log(userId)
+  try {
+    const users = await user.find({_id: userId});
+    res.send(users);
+  } catch (err) {
+    res.status(500).send({ error: true, errorMessage: err.message });
+  }
+};

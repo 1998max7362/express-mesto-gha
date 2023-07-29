601c3c2ce33cbbcf38d7d94996e99d88e248303d
Max <1998max7362@mail.ru>
2023-07-30 00:47:40 +0300

    userSchema

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
-// Слушаем 3000 порт
+const mongoose = require("mongoose");
+
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+// useNewUrlParser, useCreateIndex, useFindAndModify как я понял, больше не нужны в mongo 6
+mongoose.connect("mongodb://localhost:27017/mestodb");
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);

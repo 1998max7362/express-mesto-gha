fb0135598b1789bd5eaa5f85288988d09a6d5789
Max <1998max7362@mail.ru>
2023-07-30 03:26:34 +0300

    getCardById works

--- src/controllers/Cards/getCardById.js
+++ src/controllers/Cards/getCardById.js
@@ -1,5 +1,11 @@
 import card from "../../models/card.js";
 
-export const getCardById = () =>{
-    
-}
+export const getCardById = async (req, res) => {
+  const cardId = req.params.cardId;
+  try {
+    const cards = await card.find({ _id: cardId });
+    res.send(cards);
+  } catch (err) {
+    res.status(500).send({ error: true, errorMessage: err.message });
+  }
+};
--- src/controllers/Users/getUserById.js
+++ src/controllers/Users/getUserById.js
@@ -1,10 +1,9 @@
 import user from "../../models/user.js";
 
 export const getUserById = async (req, res) => {
-  const userId = req.params.userId
-  console.log(userId)
+  const userId = req.params.userId;
   try {
-    const users = await user.find({_id: userId});
+    const users = await user.find({ _id: userId });
     res.send(users);
   } catch (err) {
     res.status(500).send({ error: true, errorMessage: err.message });

d8d1c6b180122495272f84013056fe319b687f39
Max <1998max7362@mail.ru>
2023-07-31 01:28:12 +0300

    dдобавли MONGODB_URL не из env чтоб не ругался тест

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ import { corsAllow } from "./src/middlewares/CORS.js";
 import { auth } from "./src/middlewares/tempAuth.js";
 import { errorHandler } from "./src/middlewares/Errors/errorHandler.js";
 
-const { PORT = 3000, MONGODB_URL } = process.env;
+const { PORT = 3000, MONGODB_URL = "mongodb://0.0.0.0:27017/mestodb " } =
+  process.env;
 
 try {
   await mongoose.connect(MONGODB_URL);
@@ -24,7 +25,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(auth);
 app.use("/", router);
 app.use(errorHandler);
-
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);

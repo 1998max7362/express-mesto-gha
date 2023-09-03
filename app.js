cdc740271533152a028eb5a6678d455ab2e87469
Max <1998max7362@mail.ru>
2023-09-03 14:31:26 +0300

    cors ура

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import cors from "cors";
 import "dotenv/config";
 import { errors } from "celebrate";
 import { mongoose } from "mongoose";
@@ -15,7 +16,8 @@ import auth from "./src/middlewares/auth.js";
 import WrongRouteError from "./src/middlewares/Errors/customErrors/WrongRouteError.js";
 import { requestLogger, errorLogger } from "./src/middlewares/logger.js";
 
-const { PORT = 3000, MONGODB_URL = "mongodb://0.0.0.0:27017/mestodb " } = process.env;
+const { PORT = 3000, MONGODB_URL = "mongodb://0.0.0.0:27017/mestodb " } =
+  process.env;
 
 try {
   await mongoose.connect(MONGODB_URL);
@@ -29,7 +31,9 @@ app.use(cookieParser());
 app.disable("x-powered-by");
 app.use(limiter);
 // app.use(helmet);
-app.use(corsAllow);
+// app.use(cors());
+app.all("*", corsAllow);
+// app.use(corsAllow);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(requestLogger);
--- src/middlewares/CORS.js
+++ src/middlewares/CORS.js
@@ -1,11 +1,20 @@
-const corsAllow = (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept",
-  );
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+// eslint-disable-next-line consistent-return
+const allowedCors = [
+  "https://praktikum.tk",
+  "http://praktikum.tk",
+  "http://localhost:3000",
+  "https://localhost:3000",
+];
 
+const corsAllow = (req, res, next) => {
+  res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+
+  const { origin } = req.headers;
+  if (allowedCors.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+  res.header("Access-Control-Allow-Credentials", true);
   next();
 };
 
